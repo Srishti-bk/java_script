3567c77cebb61b118644ab30f29f98db9400f30b
Srishti-bk <bbk819398@gmail.com>
2025-11-19 15:37:12 +0545

    program

---
+++ java_script/index.js
@@ -0,0 +1,10 @@
+let number=(4,3);
+function checkNumber(a,b){
+    if(a>b)
+    {
+        console.log("a is greater");
+    } else{
+        console.log("b is greater");
+    }
+}
+checkNumber(4,3)
